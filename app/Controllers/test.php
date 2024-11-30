7573d8bea1944c84f5b015624ad97546c389b743
Binto Widodo Pramono <binto.widodo@novellpharm.com>
2024-11-30 21:20:56 +0700

    buat file untuk test abstraksi alias native PHP dg fungsi-fungsi basic seperti echo dan printf

---
+++ app/Controllers/test.php
@@ -0,0 +1,18 @@
+<?php
+//require_once __DIR__ . '/../../vendor/autoload.php'; 
+require '../../vendor/autoload.php';
+// Mengimpor class PhpVersion
+use App\Abstractions\npl_PhpVersion;
+
+// Mengecek apakah versi PHP saat ini >= 8.0.0
+if (npl_PhpVersion::versionCheck('8.0.0')) {
+    echo "PHP version is 8.0 or higher! <br/>";
+} else {
+    echo "PHP version is lower than 8.0! <br/>";
+}
+ 
+
+// Menangani deprecated feature untuk PHP versi 7.4 dan lebih tinggi
+npl_echo(npl_PhpVersion::handleDeprecation());
+
+npl_echo("<br/>-----------".npl_server());
