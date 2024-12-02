f787344b95045b6165486946c9bef3b8f63f47fb
Binto Widodo Pramono <binto.widodo@novellpharm.com>
2024-12-02 20:03:17 +0700

    Update rename nama dan tambahkan beberapa fungsi
    - npl_LINE
    - npl_dbg untuk debugging

--- app/Abstractions/npl_basic.php
+++ app/Abstractions/npl_basic.php
@@ -5,5 +5,9 @@ function npl_split($x, $y){ return explode($x, $y);}
 function npl_print_r($str){ return print_r($str);}
 
 function npl_server(){return $_SERVER['PHP_SELF'];}
-function _LINE(){return __LINE__;}
-function npl_log(){ return npl_SERVER();}
+function npl_LINE(){return __LINE__;}
+function nl_log(){ return npl_SERVER();}
+
+function npl_dbg($str){
+	npl_echo($str); exit;
+}
